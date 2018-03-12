e4b669a787f7f4d455b0151c14afc047d30665ba
Sarah Nicholson <sarahryann11@outlook.com>
2018-03-11 20:56:46 -0400

    url connection lab

--- app/src/main/java/csci490/csci490_lab_04/MainActivity.java
+++ app/src/main/java/csci490/csci490_lab_04/MainActivity.java
@@ -2,6 +2,14 @@ package csci490.csci490_lab_04;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        try {
+            urlConnection("https://jsonplaceholder.typicode.com/users");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+    public String urlConnection(String str) throws IOException
+    {
+        URL url = new URL(str);
+        URLConnection connection = url.openConnection();
+
+        InputStreamReader is = new InputStreamReader((connection.getInputStream()));
+        BufferedReader br = new BufferedReader(is);
+        String newStr = br.readLine();
+
+        Log.i("String", newStr);
+        return str;
+
+    }
+
+
 }
